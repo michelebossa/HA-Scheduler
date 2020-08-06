60ee07b01be5c7a4915ea72eda95598647fd06a3
Kendell R <KTibow@users.noreply.github.com>
2020-08-06 15:30:42 -0700

    Update daemon_input.py

--- daemon_input.py
+++ daemon_input.py
@@ -2,32 +2,31 @@ import sys
 import os
 import json
 import psutil
+
 FOLDER = "/share/ha-scheduler/"
 
-#print("Recive", sys.argv[1])
 
 input = sys.argv[1].split(":")
 list = os.listdir(FOLDER)
-reload = 'false'
+reload = "false"
 for file in list:
-   if ".json" in file:
-      filename = FOLDER + file
-      with open(filename) as json_file:
-        data = json.load(json_file)
-      if data['friendly_name'] == input[0]:
-        #print("trovato", data['entity_id'])
-        if input[1] == "enable_on":
-            data["enable"] = 'true'
-        if input[1] == "enable_off":    
-            data["enable"] = 'false'
-        with open(filename, 'w') as outfile:        
-             json.dump(data, outfile)
-        reload = 'true'
-if reload == 'true':
+    if ".json" in file:
+        filename = FOLDER + file
+        with open(filename) as json_file:
+            data = json.load(json_file)
+        if data["friendly_name"] == input[0]:
+            if input[1] == "enable_on":
+                data["enable"] = "true"
+            if input[1] == "enable_off":
+                data["enable"] = "false"
+            with open(filename, "w") as outfile:
+                json.dump(data, outfile)
+            reload = "true"
+if reload == "true":
     for process in psutil.process_iter():
-      if '/home/daemon.py' in process.cmdline():
-         print("Daemon Kill" , process.pid)
-         os.system("kill " + str(process.pid))      
+        if "/home/daemon.py" in process.cmdline():
+            print("Daemon Kill", process.pid)
+            os.system("kill " + str(process.pid))
 
     os.system("python3 /home/daemon.py &")
     print("Daemon Start")
